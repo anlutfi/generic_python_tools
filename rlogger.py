aadc25baca2c52ad260b72b07ff13294f7ca35b7
anlutfi <antonio.lutfi@gmail.com>
2024-04-14 23:55:39 -0300

    added option to exclude class header for plain functions

--- rlogger.py
+++ rlogger.py
@@ -6,9 +6,12 @@ class RLogger:
     def start(fname='execution.log', level=logging.DEBUG):
         logging.basicConfig(filename=fname, level=level)
     
-    def __init__(self, module_header, class_header, sep=':'):
+    def __init__(self, module_header, class_header='', sep=':'):
         self.sep = sep
-        self.header = self.sep.join([module_header, class_header])
+        if class_header == '':
+            self.header = module_header
+        else:
+            self.header = self.sep.join([module_header, class_header])
 
     def __call__(self, msg, exception=False):
         logger = logging.getLogger(self.sep.join([self.header,
